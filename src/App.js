10b7bc56264b8d134e024eaf57033ed0a14f6431
Oriana Abreu <oriabreu.v@gmail.com>
2020-11-30 17:37:54 +0100

    movable item changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import './App.css';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import Column from './components/Column';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
     <div className='ColumnContainer'>
       <ThemeProvider theme={theme}>
-        <Column />
+
+        <DndProvider backend={HTML5Backend}>
+
+          <Column />
+          <Column />
+          
+        </DndProvider>  
+
       </ThemeProvider>
     </div>
   );
